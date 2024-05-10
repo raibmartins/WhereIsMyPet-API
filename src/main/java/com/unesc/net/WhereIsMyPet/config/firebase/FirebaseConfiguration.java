b4bd6384d3e28ada4a10cc0545080cdfc031b234
Raí Bonassa Martins <raibmartins@hotmail.com>
2024-05-09 21:45:13 -0300

    #configs

--- src/main/java/com/unesc/net/WhereIsMyPet/config/firebase/FirebaseConfiguration.java
+++ src/main/java/com/unesc/net/WhereIsMyPet/config/firebase/FirebaseConfiguration.java
@@ -4,8 +4,11 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,12 +16,15 @@ import java.io.IOException;
 @Configuration
 public class FirebaseConfiguration {
 
+    @Autowired
+    ApplicationContext appContext;
+
     @Bean
     public FirebaseMessaging firebaseMessaging() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("serviceAccountKey.json");;
+        Resource resource = appContext.getResource("classpath:firebase/serviceAccountKey.json");
 
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
                 .build();
 
         FirebaseApp.initializeApp(options);
